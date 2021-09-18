6af025ff2b2173586ba069ae39bea7c3c4835e55
3nergetik <carbo_r86@mail.ru>
2021-09-18 21:30:53 +0500

    counter is ready

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,12 +1,12 @@
 let timer = document.getElementById("timer");
+let counter = timer.textContent;
 
 function timerMinus() {
-    let counter = timer.textContent;
     if (counter <= 0) {
         clearInterval(idInterval);
         alert("Вы победили в конкурсе!");
     } else {
-        timer.textContent = timer.textContent - 1;
+        timer.textContent--;
     }
 };
 
