3e3113b1a98353aa1e9e5ba42bebaab1cde1e6d4
3nergetik <carbo_r86@mail.ru>
2021-09-18 21:03:03 +0500

    clicker is ready

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,14 @@
+let clickerCounter = document.getElementById("clicker__counter");
+let cookie = document.getElementById("cookie");
+let counter = 0;
+
+cookie.onclick = function() {
+    counter++;
+    clickerCounter.textContent = counter;
+    if (counter % 2 == 0) {
+        cookie.width = 200;
+    } else {
+        cookie.width = 180;
+    }
+    console.log(Date());
+}
