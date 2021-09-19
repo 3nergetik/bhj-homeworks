99703781f0ee3541392e8fe3a777102184a9ec43
3nergetik <carbo_r86@mail.ru>
2021-09-19 15:27:10 +0500

    hole is ready

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,21 +1,18 @@
 let dead = document.getElementById('dead');
 let lost = document.getElementById('lost');
 
-let killedMole = 0;
-let miss = 0;
-
 getHole = (index) => {
     let currentHole = document.getElementById(`hole${index}`);
     currentHole.onclick = function() {
         if (currentHole.className.includes('hole_has-mole')) {
             dead.textContent++;
             if (dead.textContent >= 10) {
-                alert("You win!");
+                alert("Победа!");
             }
         } else {
             lost.textContent++;
             if (lost.textContent >= 5) {
-                alert("Game is over");
+                alert("Вы проиграли");
             }
         }
     }
