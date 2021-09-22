df5f5ed217393d3393954cbe1a1a7435e1192613
3nergetik <carbo_r86@mail.ru>
2021-09-22 13:19:10 +0500

    task3 is ready

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -7,12 +7,3 @@ menuLink.forEach(item => item.addEventListener('click', function(evt) {
         menuSub.forEach(sub => sub.classList.toggle('menu_active'));
     }
 }));
-/*
-menuLink.forEach(function(item) {
-    if ((item.parentElement.getElementsByClassName('menu_sub')).length == 1) {
-        console.log(item);
-        let menuSub = Array.from(item.parentElement.getElementsByClassName('menu_sub'));
-        menuSub.forEach(sub => sub.classList.add('menu_active'));
-    }
-});
-*/
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,5 +1,27 @@
-let sliderItem = Array.from(document.querySelectorAll('div img'));
+let sliderItem = Array.from(document.querySelectorAll('.slider__item'));
+let sliderArrowPrev = document.querySelector('.slider__arrow_prev');
+let sliderArrowNext = document.querySelector('.slider__arrow_next');
+let currentIndex = 0;
 
-sliderItem.forEach(item => item.onClick = function() {
-    console.log(item);
+sliderArrowPrev.addEventListener('click', function() {
+    if (currentIndex <= 0) {
+        currentIndex = sliderItem.length - 1;
+    } else {
+        currentIndex--;
+    }
+    showCurrent(currentIndex);
 });
+
+sliderArrowNext.addEventListener('click', function() {
+    if (currentIndex >= sliderItem.length - 1) {
+        currentIndex = 0;
+    } else {
+        currentIndex++;
+    }
+    showCurrent(currentIndex);
+});
+
+let showCurrent = function(index) {
+    sliderItem.forEach(item => item.classList.remove('slider__item_active'));
+    sliderItem[index].classList.add('slider__item_active');
+};
