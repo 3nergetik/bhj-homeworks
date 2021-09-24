ed335f45c2b2ddf7275cfed21906b9f4f2a07c49
3nergetik <carbo_r86@mail.ru>
2021-09-24 22:16:23 +0500

    solo is ready

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,8 +1,51 @@
 let sliderItem = Array.from(document.querySelectorAll('.slider__item'));
-let sliderArrowPrev = document.querySelector('.slider__arrow_prev');
-let sliderArrowNext = document.querySelector('.slider__arrow_next');
+let sliderArrows = Array.from(document.querySelectorAll('.slider__arrow'));
 let currentIndex = 0;
 
+let showCurrent = function(index) {
+    sliderItem.forEach(item => item.classList.remove('slider__item_active'));
+    sliderItem[index].classList.add('slider__item_active');
+};
+
+sliderArrows.forEach(item => item.addEventListener('click', function() {
+    if (item.classList.contains('slider__arrow_next')) {
+        if (currentIndex >= sliderItem.length - 1) {
+            currentIndex = 0;
+        } else {
+            currentIndex++;
+        }
+    } else {
+        if (currentIndex <= 0) {
+            currentIndex = sliderItem.length - 1;
+        } else {
+            currentIndex--;
+        }
+    }
+    showCurrent(currentIndex);
+}));
+
+/*
+sliderArrows.forEach(item => function() {
+    item.addEventListener('click', function() {
+        if (item.classList.includes('slider__arrow_prev')) {
+            if (currentIndex <= 0) {
+                currentIndex = sliderItem.length - 1;
+            } else {
+                currentIndex--;
+            }
+            showCurrent(currentIndex);
+        } else {
+            if (currentIndex >= sliderItem.length - 1) {
+                currentIndex = 0;
+            } else {
+                currentIndex++;
+            }
+            showCurrent(currentIndex);
+        }
+    });
+});
+
+/*
 sliderArrowPrev.addEventListener('click', function() {
     if (currentIndex <= 0) {
         currentIndex = sliderItem.length - 1;
@@ -20,8 +63,4 @@ sliderArrowNext.addEventListener('click', function() {
     }
     showCurrent(currentIndex);
 });
-
-let showCurrent = function(index) {
-    sliderItem.forEach(item => item.classList.remove('slider__item_active'));
-    sliderItem[index].classList.add('slider__item_active');
-};
+*/
