378dcb43592f3daebc523f8bcf531c6dbebc0efc
3nergetik <carbo_r86@mail.ru>
2021-10-06 21:56:44 +0500

    ready

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,23 @@
+const tooltips = document.querySelectorAll('.has-tooltip');
+let emptyDiv = document.createElement('div');
+
+emptyDiv.classList.add('tooltip');
+
+for (let tooltip of tooltips) {
+    tooltip.addEventListener('click', (evt) => {
+        evt.preventDefault();
+
+        let left = Math.round(tooltip.getBoundingClientRect().left);
+        let bottom = Math.round(tooltip.getBoundingClientRect().bottom);
+
+        emptyDiv.classList.toggle('tooltip_active');
+
+        emptyDiv.textContent = tooltip.getAttribute("title");
+        emptyDiv.setAttribute("style", `left:${left}px; top:${bottom}px`);
+        document.body.appendChild(emptyDiv);  
+
+        window.addEventListener('scroll', () => {
+            document.body.removeChild(emptyDiv);
+        });
+    });
+};
