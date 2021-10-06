f39093e0de2d97792a7b7b7c86e0959e7b660068
3nergetik <carbo_r86@mail.ru>
2021-10-07 00:52:56 +0500

    cart is in progress

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,31 +1,45 @@
 let productQuantityControls = document.querySelectorAll('.product__quantity-controls');
 let cartProducts = document.querySelector('.cart__products');
-let addButtons = document.querySelectorAll('.product__add');
+let product = document.querySelectorAll('.product');
 
 // Обработчик на кнопки + и -
-for (let item of productQuantityControls) {
-    let itemButtons = item.querySelectorAll('.product__quantity-control');
-    itemButtons.forEach(item => item.addEventListener('click', () => {
-        if (item.classList.contains('product__quantity-control_dec')) {
+for (let productQuantityControl of productQuantityControls) {
+    let decButton = productQuantityControl.querySelector('.product__quantity-control_dec');
+    let incButton = productQuantityControl.querySelector('.product__quantity-control_inc');
+    let value = productQuantityControl.querySelector('.product__quantity-value');
 
-        }
-    }));
-};
+    decButton.addEventListener('click', () => {
+        if (value.textContent > 1) {
+            value.textContent--;
+        };
+    });
 
-// Обработчик на кнопки "Добавить в корзину"
-for (let addButton of addButtons) {
-    addButton.addEventListener('click', () => {
-        // Вызываем функцию добавления товара в корзину
-        // В аргументы передаем количество товара и копируем его картинку
-        addProduct(1);
+    incButton.addEventListener('click', () => {
+        value.textContent++;
     });
 };
+
+for (let productItem of product) {
+
+    let addButtons = productItem.querySelectorAll('.product__add');
+    
+    // Обработчик на кнопки "Добавить в корзину"
+    for (let addButton of addButtons) {
+        addButton.addEventListener('click', () => {
+            // Вызываем функцию добавления товара в корзину
+            // В аргументы передаем количество товара и копируем его картинку
+            addProduct(1);
+        });
+    };  
+};
+
+
 
 // Функция для добавления товара в корзину
 function addProduct(count) {
 
     // Выполняем проверку, есть ли подобный товар в корзине
-    if () {
+    if (count) {
         // и если есть, то просто увеличиваем счетчик
 
     } else {
