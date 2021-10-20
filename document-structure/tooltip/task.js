8c4951c552a5e1f433658403f0b5089a6a996d9c
3nergetik <carbo_r86@mail.ru>
2021-10-20 16:30:41 +0500

    interest is ready

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,11 +1,17 @@
 const tooltips = document.querySelectorAll('.has-tooltip');
 let emptyDiv = document.createElement('div');
+let prevBottom;
+let prevLeft;
 
 emptyDiv.classList.add('tooltip');
 
 for (let tooltip of tooltips) {
     tooltip.addEventListener('click', (evt) => {
         evt.preventDefault();
+
+        if (prevBottom && bottom === prevBottom && left === prevLeft) {
+            emptyDiv.classList.toggle('tooltip_active');
+        };
 
         let left = Math.round(tooltip.getBoundingClientRect().left);
         let bottom = Math.round(tooltip.getBoundingClientRect().bottom);
@@ -14,10 +20,10 @@ for (let tooltip of tooltips) {
 
         emptyDiv.textContent = tooltip.getAttribute("title");
         emptyDiv.setAttribute("style", `left:${left}px; top:${bottom}px`);
-        document.body.appendChild(emptyDiv);  
+        document.body.appendChild(emptyDiv);
 
         window.addEventListener('scroll', () => {
-            document.body.removeChild(emptyDiv);
+            emptyDiv.classList.remove('tooltip_active');
         });
     });
 };
